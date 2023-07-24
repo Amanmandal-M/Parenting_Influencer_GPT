e1f9e60a57932e6355ce8c5fcec69ac801d025a1
Amanmandal-M <amanmandal644@gmail.com>
2023-07-24 17:49:06 +0530

    Signup is working

--- Frontend/influencer-app/src/app/signup/signup.component.ts
+++ Frontend/influencer-app/src/app/signup/signup.component.ts
@@ -2,17 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import Swal from 'sweetalert2'; // Import Swal class
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
   signupForm: FormGroup = new FormGroup({});
   isLoading: boolean = false;
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.signupForm = this.formBuilder.group({
@@ -25,7 +30,7 @@ export class SignupComponent implements OnInit {
     });
   }
 
-  onSubmit() {
+  async onSubmit() {
     if (this.signupForm.invalid) {
       return;
     }
@@ -33,18 +38,21 @@ export class SignupComponent implements OnInit {
     this.isLoading = true;
     const userData = this.signupForm.value;
     // Replace the API endpoint with your actual backend URL
-    const apiUrl = 'https://parenting-influencer-backend.onrender.com/user/register';
+    const apiUrl =
+      'https://parenting-influencer-backend.onrender.com/user/register';
 
     this.http.post(apiUrl, userData).subscribe(
       (response: any) => {
         this.isLoading = false;
         this.signupForm.reset();
         this.showSuccessAlert(); // Call the success alert method
+        this.router.navigateByUrl('/login'); // Navigate to the login page after successful registration
       },
       (error) => {
         this.isLoading = false;
         if (error.status === 401) {
           this.showUserExistsAlert(); // Call the user already exists alert method
+          this.router.navigateByUrl('/login');
         } else {
           this.showErrorAlert(); // Call the general error alert method
         }
@@ -57,10 +65,10 @@ export class SignupComponent implements OnInit {
     Swal.fire({
       icon: 'success',
       title: 'Success!',
-      width: "25%",
+      width: '25%',
       text: 'User registration successful!',
-      timer: 2000, // Auto close the alert after 2 seconds
-      showConfirmButton: false
+      timer: 2500, // Auto close the alert after 2 seconds
+      showConfirmButton: false,
     });
   }
 
@@ -68,11 +76,11 @@ export class SignupComponent implements OnInit {
   showUserExistsAlert() {
     Swal.fire({
       icon: 'error',
-      title: 'Error!',
-      width: "22%",
+      title: 'Please Login',
+      width: '25%',
       text: 'User is already registered!',
-      timer: 2000, // Auto close the alert after 2 seconds
-      showConfirmButton: false
+      timer: 2500, // Auto close the alert after 2 seconds
+      showConfirmButton: false,
     });
   }
 
@@ -81,13 +89,10 @@ export class SignupComponent implements OnInit {
     Swal.fire({
       icon: 'error',
       title: 'Error!',
-      width:"22%",
+      width: '25%',
       text: 'User registration failed!',
-      timer: 2000, // Auto close the alert after 2 seconds
-      showConfirmButton: false
+      timer: 2500, // Auto close the alert after 2 seconds
+      showConfirmButton: false,
     });
   }
-
 }
-
-
