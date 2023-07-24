0489702ac5f67e28d364c2bd9e57e1be854494a2
Amanmandal-M <amanmandal644@gmail.com>
2023-07-24 20:34:49 +0530

    Login is working fine

--- Frontend/influencer-app/src/app/login/login.component.ts
+++ Frontend/influencer-app/src/app/login/login.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import Swal from 'sweetalert2'; // Import Swal class
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup = new FormGroup({}); // Initialize the form group with an empty object
+  loginForm: FormGroup = new FormGroup({});
   isLoading: boolean = false;
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
@@ -28,18 +34,71 @@ export class LoginComponent implements OnInit {
     this.isLoading = true;
     const userData = this.loginForm.value;
     // Replace the API endpoint with your actual backend URL
-    const apiUrl = 'http://192.168.1.4:8080/user/login';
+    const apiUrl =
+      'https://parenting-influencer-backend.onrender.com/user/login';
 
     this.http.post(apiUrl, userData).subscribe(
       (response: any) => {
         this.isLoading = false;
-        alert('Login successful!');
-        this.loginForm.reset();
+        response.Data = JSON.parse(response.Data)
+
+        // Save the user data in session storage
+        sessionStorage.setItem('userData', JSON.stringify(response));
+
+        // Show success alert
+        this.showSuccessAlert();
+
+        // Navigate to the home page
+        this.router.navigateByUrl('/home');
       },
       (error) => {
         this.isLoading = false;
-        alert('Login failed. Please check your email and password.');
+        if (error.status === 401) {
+          this.showUserNotFoundAlert(); // Call the user not found alert method
+        this.router.navigateByUrl('/signup');
+        } else {
+          this.showErrorAlert(); // Call the general error alert method
+        }
       }
     );
+  }
+
+  // Method to show success alert
+  showSuccessAlert() {
+    Swal.fire({
+      icon: 'success',
+      title: 'Success!',
+      width: '25%',
+      text: 'Login successful!',
+      timer: 2000, // Auto close the alert after 2.5 seconds
+      showConfirmButton: false,
+    });
+  }
+
+  // Method to show error alert for user not found
+  showUserNotFoundAlert() {
+    Swal.fire({
+      icon: 'error',
+      title: 'User Not Found',
+      width: '25%',
+      text: 'User not found. Please sign up first!',
+      timer: 2500, // Auto close the alert after 2.5 seconds
+      showConfirmButton: false,
+    }).then(() => {
+      // Navigate to the signup page
+      this.router.navigateByUrl('/signup');
+    });
+  }
+
+  // Method to show general error alert
+  showErrorAlert() {
+    Swal.fire({
+      icon: 'error',
+      title: 'Error!',
+      width: '25%',
+      text: 'Login failed. Please check your email and password.',
+      timer: 2500, // Auto close the alert after 2.5 seconds
+      showConfirmButton: false,
+    });
   }
 }
